e14488f8fc1395ecbc2134d308aa3f351fa17eaf
Mohana Varshitha <mohanavarshitha@icloud.com>
2023-11-13 11:43:11 +0530

    create sign up API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express() ;
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server listening to port 3000');
 });
@@ -20,3 +24,4 @@ app.listen(3000, () => {
 // api route
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
